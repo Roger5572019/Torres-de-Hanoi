a66563f242fc99c36ed790e45eada40bcf63fa8a
Roger557 <roger5572019@outlook.com>
2025-05-01 23:19:43 -0600

    Se agregró y configuró la librería allegro5

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+
+int main()
+{
+    al_init();
+    al_install_keyboard();
+    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);
+    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
+    ALLEGRO_DISPLAY *disp = al_create_display(320, 500);
+    ALLEGRO_FONT *font = al_create_builtin_font();
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+    ALLEGRO_EVENT event;
+    bool redraw = true;
+    
+    while (1)
+    {
+        al_wait_for_event(queue, &event);
+        if (event.type == ALLEGRO_EVENT_TIMER)
+            redraw = true;
+        else if ((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+            break;
+    }
+}
