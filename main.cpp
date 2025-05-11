64e3741cb42d76a352f43a024ca821b813e7d4ce
Roger557 <roger5572019@outlook.com>
2025-05-10 20:32:37 -0600

    Hello World con arial

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
-
+#include <allegro5/allegro_ttf.h>
+#include <iostream>
+using namespace std;
 int main()
 {
     al_init();
     al_install_keyboard();
 
-    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue(); // inicializar cola de eventos
-    ALLEGRO_DISPLAY *disp = al_create_display(320, 500);  // pantalla
+    al_init_font_addon();
+    al_init_ttf_addon();
+
+    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
+    ALLEGRO_DISPLAY *disp = al_create_display(320, 500);
     ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);
-    ALLEGRO_FONT *font = al_create_builtin_font(); // fuente
-    // Registrar eventos a queque (cola de eventos)
+
+    ALLEGRO_FONT *font = al_load_font("C:/Windows/Fonts/arial.ttf", 24, 0);
+    if (!font)
+    {
+        printf("Error: no se pudo cargar la fuente.\n");
+        return -1;
+    }
+
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
+
     ALLEGRO_EVENT event;
     bool redraw = true;
     al_start_timer(timer);
+
     while (1)
     {
         al_wait_for_event(queue, &event);
